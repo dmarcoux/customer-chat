0639a91739df97cc9764e7cf2edff0fcc45f1b80
Dany Marcoux <git@dmarcoux.com>
2024-10-25 21:19:44 +0200

    Redirect root URL to support_cases/

--- chat/urls.py
+++ chat/urls.py
@@ -3,19 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("support_cases/", views.SupportCasesView.as_view(), name="support_cases"),
+    path("", views.SupportCasesView.as_view(), name="support_cases"),
     path(
-        "support_cases/new",
+        "new/",
         views.SupportCaseNewView.as_view(),
         name="support_cases_new",
     ),
     path(
-        "support_cases/<int:id>",
+        "<int:id>/",
         views.SupportCaseShowView.as_view(),
         name="support_cases_show",
     ),
     path(
-        "support_cases/<int:id>/messages/",
+        "<int:id>/messages/",
         views.SupportCaseMessagesView.as_view(),
         name="messages_create",
     ),
--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path("", include("chat.urls")),
+    path("", RedirectView.as_view(pattern_name="support_cases")),
+    path("support_cases/", include("chat.urls")),
     path(
         "accounts/login/",
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
